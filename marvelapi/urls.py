c46fd30f2e205cc6a7c74edd9ed6fe8643ac5cf5
nacpacheco <nahara.cpacheco@gmail.com>
2019-12-01 18:27:49 -0300

    Implemented Character related endpoints

--- marvelapi/urls.py
+++ marvelapi/urls.py
@@ -3,9 +3,16 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'characters', views.CharacterViewSet)
+router.register(r'characters', views.FullCharacterViewSet)
+router.register(r'^characters/(?P<id>.+)/$', views.CharacterViewSet, base_name='Character')
+router.register(r'^characters/(?P<id>.+)/comics', views.ComicViewSet, base_name='Comics')
+router.register(r'^characters/(?P<id>.+)/stories', views.StorieViewSet, base_name='Storie')
+router.register(r'^characters/(?P<id>.+)/events', views.EventViewSet, base_name='Events')
+router.register(r'^characters/(?P<id>.+)/series', views.SerieViewSet, base_name='Series')
+
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
--- marvelapi/views.py
+++ marvelapi/views.py
@@ -1,8 +1,45 @@
-from rest_framework import viewsets
-from .serializers import CharacterSerializer
+from rest_framework import viewsets, generics
+from .serializers import *
 from .models import *
 
 
-class CharacterViewSet(viewsets.ModelViewSet):
+class FullCharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all().order_by('name').select_related()
     serializer_class = CharacterSerializer
+
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return Character.objects.all().filter(id=id).select_related()
+
+class ComicViewSet(viewsets.ModelViewSet):
+    serializer_class = ComicSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return Comic.objects.all().filter(character_id=id)
+
+class StorieViewSet(viewsets.ModelViewSet):
+    serializer_class = StorieSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return Storie.objects.all().filter(character_id=id)
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return Event.objects.all().filter(character_id=id)
+
+class SerieViewSet(viewsets.ModelViewSet):
+    serializer_class = SerieSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id']
+        return Serie.objects.all().filter(character_id=id)
+
