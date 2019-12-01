d267f3bf4eff6463cee9ca434046336fc1b0d37a
nacpacheco <nahara.cpacheco@gmail.com>
2019-12-01 16:21:58 -0300

    Simple Get Characters working

--- marvelapi/models.py
+++ marvelapi/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Characters(models.Model):
+class Character(models.Model):
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=100)
     modified =  models.DateTimeField(auto_now=True)
--- marvelapi/views.py
+++ marvelapi/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import CharacterSerializer
+from .models import Character
+
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character.objects.all().order_by('name')
+    serializer_class = CharacterSerializer
